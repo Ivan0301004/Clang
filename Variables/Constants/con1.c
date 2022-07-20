7d1fe86a00eb6f64c5dcd0cce1fd29660f20cbef
Ivan0301004 <ignizbones@gmail.com>
2022-07-20 15:54:17 -0500

    new

---
+++ Variables/Constants/con1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// We can define a constant with MACROS as follows
+
+#define PI 3.1412592
+#define E 3.71
+// As well we can define functions with macros as follows
+#define ADD(x, c) x + c
+
+int main() { 
+  
+  return 0; 
+}
