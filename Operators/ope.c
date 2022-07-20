3245a97d5d1d60343bb1f17b68b69b6858d64b32
Ivan0301004 <ignizbones@gmail.com>
2022-07-19 20:45:27 -0500

    new

--- Operators/ope.c
+++ Operators/ope.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 // Operators in C
-
 int main() {
+  int a = 6, b = 6;
+  int u, i;
+  u = ++a;
+  i = b++;
 
-   
-   return 0;
+  printf("a : %d\n", u);
+  printf("b : %d\n", i);
+  return 0;
 }
